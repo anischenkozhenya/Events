bb0a06d36df8f204b918ac1b50e50c3b49797de5
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-09-17 17:26:29 +0300

    +Task1 MVP

---
+++ Task1/Model.cs
@@ -0,0 +1,10 @@
+﻿namespace Task1
+{
+    class Model
+    {
+        public string MyMethod(string str)
+        {
+            return "Текст в TextBox: '" +str+"'";
+        }
+    }
+}
---
+++ Task1/Presenter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Presenter
+    {
+        Model model = null;
+        MainWindow mainWindow = null;
+        public Presenter(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+            model = new Model();
+            this.mainWindow.MyEvent += MainWindow_MyEvent;
+        }
+        private void MainWindow_MyEvent(object sender, EventArgs e)
+        {
+            this.mainWindow.TBMain.Text = model.MyMethod(this.mainWindow.TBMain.Text);
+        }
+    }
+}
