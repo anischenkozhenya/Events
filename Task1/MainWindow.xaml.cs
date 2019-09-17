cf123fecd14d07d1dd69dc5c02c9fe446f53205f
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-09-17 19:05:45 +0300

    +task1 full

--- Task1/MainWindow.xaml.cs
+++ Task1/MainWindow.xaml.cs
@@ -12,7 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+//Реализовать паттерн MVP, добавьте метод доступа
+//add и remove к событию
 namespace Task1
 {
     /// <summary>
@@ -21,13 +22,17 @@ namespace Task1
     public partial class MainWindow : Window
     {
         Presenter presenter = null;
+        public EventHandler MyEvent = null;
+        public event EventHandler MyEvemt
+        {
+            add { MyEvent += value; }
+            remove { MyEvent -= value; }
+        }
         public MainWindow()
         {
             InitializeComponent();
             presenter = new Presenter(this);
-        }
-        public event EventHandler MyEvent = null;
-
+        }  
         private void BtnDoSomething_Click(object sender, RoutedEventArgs e)
         {
             this.MyEvent(sender, e);
