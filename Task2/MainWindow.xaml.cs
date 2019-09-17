8b959c23e4499e0c59b485341a9565de2b985b1b
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-09-17 19:37:13 +0300

    +Task2 Wpf

---
+++ Task2/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+//using System.Windows.Shapes;
+//Используя Visual Studio, создайте проект по шаблону WPF Application.
+//Создайте программу секундомер. Требуется выводить показания секундомера в окне. Окно имеет
+//кнопки запуска, останова и сброса секундомера. Для реализации секундомера используйте паттерн MVP. 
+namespace Task2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
